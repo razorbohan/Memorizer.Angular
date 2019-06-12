6f7707ae2614c442853311e43e30239a4bdafa87
RaZoR <razorbohan@yahoo.com>
2019-06-12 17:58:09 +0300

    editable find form

--- src/app/shared/modals/find/find.component.ts
+++ src/app/shared/modals/find/find.component.ts
@@ -7,6 +7,7 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
 import * as moment from 'moment';
 import { MemoService } from '../../services/memo.service';
 import { Memo } from '../../models/memo';
+import { FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
 
 export const MY_FORMATS = {
 	parse: {
@@ -33,6 +34,8 @@ export class FindMemoComponent implements OnInit {
 	message: string;
 	isLoading: boolean;
 
+	controls: FormArray;
+
 	dataSource: MatTableDataSource<Memo>;
 	displayedColumns: string[] = ['id', 'question', 'answer', 'repeatDate', 'postponeLevel', 'scores'];
 
@@ -44,22 +47,22 @@ export class FindMemoComponent implements OnInit {
 		private modalRef: BsModalRef
 	) { }
 
-	ngOnInit() {
-		this.getMemos();
+	async ngOnInit() {
+		await this.getMemos();
 		this.dataSource.sort = this.sort;
 		this.dataSource.paginator = this.paginator;
 	}
 
-	applyFilter(filterValue: string) {
+	public applyFilter(filterValue: string) {
 		this.dataSource.filter = filterValue.trim().toLowerCase();
 	}
 
-	async getMemos() {
+	private async getMemos() {
 		try {
 			this.isLoading = true;
 
 			let memos = await this.memoService.getMemos();
-			memos = memos.slice(0, 100);
+			this.generateFormControls(memos);
 			this.dataSource = new MatTableDataSource<Memo>(memos);
 		} catch (error) {
 			console.error(error.statusText);
@@ -70,7 +73,44 @@ export class FindMemoComponent implements OnInit {
 		}
 	}
 
-	hide() {
-		this.modalRef.hide()
+	private generateFormControls(memos: Memo[]) {
+		const toGroups = memos.map(entity => {
+			return new FormGroup({
+				id: new FormControl(entity.id, Validators.required),
+				question: new FormControl(entity.question, Validators.required),
+				answer: new FormControl(entity.answer, Validators.required),
+				repeatDate: new FormControl(entity.repeatDate, Validators.required),
+				postponeLevel: new FormControl(entity.postponeLevel, Validators.required),
+				scores: new FormControl(entity.scores, Validators.required)
+			}, { updateOn: "blur" });
+		});
+		this.controls = new FormArray(toGroups);
+	}
+
+	public updateField(index: number, memo: Memo, field: string) {
+		const control = this.getControl(index, field);
+		if (control.valid) {
+			try {
+				this.isLoading = true;
+				memo = control.value
+				let tt = memo;
+				console.log(tt);				
+				
+				//this.message = await this.memoService.updateMemo(this.currentMemo);
+			} catch (error) {
+				//this.message = new Message(error.message, 'danger');
+			} finally {
+				this.isLoading = false;
+			}
+			//this.core.update(index, field, control.value);
+		}
+	}
+
+	public getControl(index: number, fieldName: string) {
+		return this.controls.at(index).get(fieldName) as FormControl;
+	}
+
+	public hide() {
+		this.modalRef.hide();
 	}
 }
--- src/app/shared/modals/find/find.module.ts
+++ src/app/shared/modals/find/find.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMomentDateModule } from '@angular/material-moment-adapter'
 import {
@@ -11,14 +11,25 @@ import {
 import { FindMemoComponent } from './find.component';
 import { BigLoaderComponent } from '../../components/big-loader/big-loader.component';
 
+import { EditableComponent } from './editable/editable.component';
+import { ViewModeDirective } from './editable/view-mode.directive';
+import { EditModeDirective } from './editable/edit-mode.directive';
+import { EditableOnEnterDirective } from './editable/edit-on-enter.directive';
+
 @NgModule({
   declarations: [
     FindMemoComponent,
-    BigLoaderComponent
+    BigLoaderComponent,
+
+    EditableComponent,
+    ViewModeDirective,
+    EditModeDirective,
+    EditableOnEnterDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatTableModule,
     MatInputModule,
